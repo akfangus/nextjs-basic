8660fc505fd38ad0f39057fc38814bf095f02cf7
akfangus <akfanakfan@naver.com>
2023-04-20 03:56:10 +0900

    not-found를 지정해주었습니다

---
+++ src/app/not-found.tsx
@@ -0,0 +1,3 @@
+export default function NotFoundPage() {
+  return <div>페이지를 찾을수 없음!</div>;
+}
---
+++ src/app/products/not-found.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProductNotFound() {
+  return <div>제품을 찾을 수 없음!</div>;
+}
